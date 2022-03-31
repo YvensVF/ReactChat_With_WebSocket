8777f97f75922d31f73c112fd8ee5c8e0a52ac41
Yvens <yvensfarias@gmail.com>
2022-03-31 14:39:56 -0300

    implementação do chat completa

--- src/Chat.js
+++ src/Chat.js
@@ -1,42 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import uuid from "../node_modules/uuid/dist/v4";
+
+const myId = uuid();
+const socket = io("http://localhost:6060", { transports: ["websocket"] });
+socket.on("connect", () => console.log("[IO] Connect => New connection"));
 
 function Chat() {
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const handleNewMessage = (newMessage) =>
+      setMessages([...messages, newMessage]);
+    socket.on("chat.message", handleNewMessage);
+    return () => socket.off("chat.message", handleNewMessage);
+  }, [messages]);
 
   const handleFormSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (message.trim()) {
-      setMessages([...messages, {
-        id: 1,
-        message
-      }])
-      setMessage('')
+      socket.emit("chat.message", {
+        id: myId,
+        message,
+      });
+      setMessage("");
     }
-  }
+  };
 
   const handleInputChange = (e) => {
-    setMessage(e.target.value)
-
-  }
+    setMessage(e.target.value);
+  };
   return (
     <main className="container">
       <ul className="list">
-        {messages.map(m => (
-        <li className="list__item list__item--mine">
-          <span className="message message--mine" key={m.id}>
-            {m.message}
-          </span>
-        </li>
+        {messages.map((m) => (
+          <li 
+            className={`list__item list__item--${m.id === myId ? 'mine' : 'other'}`} 
+            key={m.id}
+          >
+            <span className={`message message--${m.id === myId ? 'mine' : 'other'}`}>{m.message}</span>
+          </li>
         ))}
       </ul>
       <form className="form" onSubmit={handleFormSubmit}>
-        <input 
-        className="form__field" 
-        placeholder="Digite um novo texto aqui"
-        type="text"
-        onChange={handleInputChange}
-        value={message}
+        <input
+          className="form__field"
+          placeholder="Digite um novo texto aqui"
+          type="text"
+          onChange={handleInputChange}
+          value={message}
         />
       </form>
     </main>
